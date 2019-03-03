0a1f57935b776628f8a3d6e12e1f0df45abcdbdb
unknown <programmingysl@gmail.com>
2019-03-03 15:34:59 -0500

    completed the assignment

--- main.ts
+++ main.ts
@@ -15,7 +15,7 @@ class Game {
         //Add the text
         nameCell.appendChild(nameText);
         rateCell.appendChild(rateText);
-        
+
         //Add cells to row
         newRow.appendChild(nameCell);
         newRow.appendChild(rateCell);
@@ -25,12 +25,17 @@ class Game {
     }
 }
 
-//Declare the varible to store the game.
+//Declare the variable to store the game.
 let newGame: Game;
 
 //The function that adds the game
 function addGame() :void {
 
     //YOUR CODE GOES HERE
+
+    let g = new Game((<HTMLInputElement>document.getElementById("name")).value, Number((<HTMLInputElement>document.getElementById("rating")).value));
+
+    let row = g.createListNode();
+    document.getElementById("games").appendChild(row);
 
 }
